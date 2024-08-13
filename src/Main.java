51a377928fb4b7a4f0a44797e2621cd055a5d407
Gustavo da Rosa <guga-rosa13@hotmail.com>
2024-08-13 14:04:40 -0300

    Desafio de Projeto - Gustavo da Rosa

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente gustavo = new Cliente();
+		gustavo.setNome("Gustavo");
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Conta cc = new ContaCorrente(gustavo);
+		Conta poupanca = new ContaPoupanca(gustavo);
 
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
+		
+		cc.depositar(500);
+		poupanca.sacar(50);
 		
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
